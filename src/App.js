287bc5ecf3048efd9128ed6f322c41d1ac859888
PanchoCorrea <correafrancisco2003@gmail.com>
2023-03-29 13:36:01 -0300

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+import { Button } from 'react-bootstrap';
+
+function App(){
+  
+  return (
+
+  
+    <div>
+      <form onSubmit={presion}>
+        <p>Ingrese :  <br></br>
+          <input type="letter" name="valor1" />
+        </p>
+        <p>
+          <input type="submit" value="ENTER" />
+        </p>
+      </form>
+    </div>
+  
+  );
+
+  function presion(e) {
+    e.preventDefault();
+   let palabra=(e.target.valor1.value);
+   
+   let vocales = ['a','e','i','o','u'];
+   let contador = 0;
+   
+   for(let i = 0; i < palabra.length; i++){
+       for(let l = 0; l < vocales.length; l++){
+          
+       if(palabra[i] == vocales[l]){contador++;}
+   
+       }
+   }
+   alert('Cantidad de vocales encontradas: ' +contador);
+   }
+}
+  
 export default App;
